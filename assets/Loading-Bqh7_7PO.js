a8627070999aebc2853de4850e7ba37a12d371a7
mihoag <mihoag@users.noreply.github.com>
2025-06-28 10:20:58 +0000

    deploy: 4cd24918886ceef0a3d300ef6f0e96c61dffce78

--- assets/Loading-Bqh7_7PO.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Cix6GTrn.js";import{a as s}from"./App-Dk_tdKar.js";const i=()=>e.jsx("div",{className:"max-w-4xl mx-auto px-4 py-8",children:e.jsx("div",{className:"flex justify-center items-center h-64",children:e.jsx("div",{className:"animate-pulse text-gray-400",children:s("lbl.loading")})})});export{i as L};
