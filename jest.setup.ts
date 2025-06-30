37df3c939fd40b817dcd50f7963db48132dfb4ce
minhhoang <hoang.lm@teko.vn>
2025-06-30 20:46:40 +0700

    [SCRUM-195] Write UT for homepage

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+// Add TextEncoder/TextDecoder polyfills for React Router
+// For Node.js environments
+import { TextEncoder, TextDecoder } from 'util';
+
+// Use type assertions to solve the type compatibility issue
+global.TextEncoder = TextEncoder as typeof global.TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
