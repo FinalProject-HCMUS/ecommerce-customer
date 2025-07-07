847a73ef708330220c569663067c503d852b0741
21120447 <haonhat2729@gmail.com>
2025-07-07 21:22:09 +0700

    feat: add coze chatbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/index';
+import CozeChat from './CozeChat';
 
 const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <CozeChat/>
+      <RouterProvider router={router} />
+    </>
+  )
 };
 
 export default App;
